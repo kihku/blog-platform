108dcc76b4d11ee9f5f8ecb275122a742a72084b
Phạm Trần Hiền Dung <pthdung2903@gmail.com>
2024-10-05 20:28:30 +0300

    Add simple layout for menu
    haven't implement css, responsible, menu router, btn etc.

---
+++ blog-project/src/app/flashcards/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Typography } from "@material-tailwind/react";
+
+export default function Flashcards() {
+  return (
+    <div className="p-5 p-y-10 lg:px-24">
+      <Typography type="h1">Recent</Typography>
+    </div>
+  );
+}
