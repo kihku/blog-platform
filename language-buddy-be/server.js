e7fb17d85daab68d37da703d8b4a19794d1aa1be
Phạm Trần Hiền Dung <pthdung2903@gmail.com>
2024-11-13 15:10:11 +0200

    add eslint, prettier

--- language-buddy-be/server.js
+++ language-buddy-be/server.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
 const app = express();
+dotenv.config({ path: './config.env' });
 
-app.use(morgan("dev"));
-app.use(express.json())
-const userRoute = require("./routes/userRoutes");
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use("/users", userRoute);
+app.use(express.json());
+const userRoute = require('./routes/userRoutes');
 
-const port = 3000;
+app.use('/users', userRoute);
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
