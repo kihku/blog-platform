e61d1a1ae4fefadf06eaf1dd3efbc727e800b4ea
Phạm Trần Hiền Dung <pthdung2903@gmail.com>
2024-11-13 15:10:11 +0200

    add eslint, prettier

--- language-buddy-be/routes/userRoutes.js
+++ language-buddy-be/routes/userRoutes.js
@@ -1,10 +1,7 @@
-const express = require("express");
-const { getUser, checkId } = require("../controllers/userController");
+const express = require('express');
+const { getUser, checkId } = require('../controllers/userController');
 
 const router = express.Router();
-
-router.param("id", checkId);
-
-router.route("/:id").get(getUser);
-
+router.param('id', checkId);
+router.route('/:id').get(getUser);
 module.exports = router;
